33335a2cce3743fae8d6773a32b04b593a5744ca
Steeve Santiago <steeve.santiago@externel.be>
2023-06-09 14:10:20 +0200

    first commit

--- src/AspNetCore.HealthChecks.Aggregator.UI/Startup.cs
+++ src/AspNetCore.HealthChecks.Aggregator.UI/Startup.cs
@@ -1,56 +1,62 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using System;
 
-namespace AspNetCore.HealthChecks.Aggregator.UI
+namespace AspNetCore.HealthChecks.Aggregator.UI;
+
+public class Startup
 {
-    public class Startup
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
     {
-        public IConfiguration Configuration { get; }
+        Configuration = configuration;
+    }
 
-        public Startup(IConfiguration configuration)
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddHealthChecksUI(settings =>
         {
-            Configuration = configuration;
-        }
-        
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddHealthChecksUI(settings =>
+            settings.SetHeaderText(Configuration.GetHealthCheckOptions().HeaderText);
+            settings.SetEvaluationTimeInSeconds(Configuration.GetHealthCheckOptions().PollingInterval);
+
+            Configuration.GetHealthCheckServices().ForEach(service =>
             {
-                settings.SetHeaderText(Configuration.GetHealthCheckOptions().HeaderText);
-                settings.SetEvaluationTimeInSeconds(Configuration.GetHealthCheckOptions().PollingInterval);
-
-                Configuration.GetHealthCheckServices().ForEach(service =>
-                {
-                    settings.AddHealthCheckEndpoint(service.Name, service.Url);
-                });
-            }).AddInMemoryStorage();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapHealthChecksUI(setup =>
-                {
-                    setup.ApiPath = Configuration.GetHealthCheckOptions().ApiPath;
-                    setup.UIPath = Configuration.GetHealthCheckOptions().UIPath;
-                });
+                settings.AddHealthCheckEndpoint(service.Name, service.Url);
             });
+        }).AddInMemoryStorage();
+
+        services.AddHealthChecks()
+            .AddCheck(name: "Random", () => DateTime.UtcNow.Second % 2 == 0 ? HealthCheckResult.Healthy() : HealthCheckResult.Unhealthy());
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
         }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapHealthChecksUI(setup =>
+            {
+                setup.ApiPath = Configuration.GetHealthCheckOptions().ApiPath;
+                setup.UIPath = Configuration.GetHealthCheckOptions().UIPath;
+            });
+
+            endpoints.MapHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+        });
     }
 }
--- src/ProductService/Startup.cs
+++ src/ProductService/Startup.cs
@@ -1,34 +1,38 @@
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.ServiceProcess;
 
-namespace ProductService
+namespace ProductService;
+
+public class Startup
 {
-    public class Startup
+    public void ConfigureServices(IServiceCollection services)
     {
-        public void ConfigureServices(IServiceCollection services)
+        services.AddHealthChecks()
+            .AddWindowsServiceHealthCheck("Fax", s => s.Status == ServiceControllerStatus.Running)
+            .AddCheck(name: "Random", () => DateTime.UtcNow.Second % 2 == 0 ? HealthCheckResult.Healthy() : HealthCheckResult.Unhealthy());
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
         {
-            services.AddHealthChecks().AddProcessAllocatedMemoryHealthCheck(100, name: "AllocatedMemory");
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+            endpoints.MapHealthChecks("/health", new HealthCheckOptions
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions()
-                {
-                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-                });
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
             });
-        }
+        });
     }
 }
